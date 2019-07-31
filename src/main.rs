5bb22a2fb389baa7d177b4925ef3da9840e02246
Ruud van Asseldonk <dev@veniogames.com>
2019-07-31 21:14:34 +0200

    Put a black bitmap on the screen

--- src/main.rs
+++ src/main.rs
@@ -7,19 +7,40 @@
 
 extern crate gio;
 extern crate gtk;
+extern crate gdk_pixbuf;
 
 use gio::prelude::*;
 use gtk::prelude::*;
+use gdk_pixbuf as gdk;
 
 use std::env;
 
+fn build_canvas() -> Option<gdk::Pixbuf> {
+    let has_alpha = false;
+    let bits_per_sample = 8;
+    let width = 1280;
+    let height = 720;
+    gdk::Pixbuf::new(
+        gdk::Colorspace::Rgb,
+        has_alpha,
+        bits_per_sample,
+        width,
+        height,
+    )
+}
+
 fn build_ui(application: &gtk::Application) {
     let window = gtk::ApplicationWindow::new(application);
+
+    let canvas = build_canvas();
+    let image = gtk::Image::new_from_pixbuf(canvas.as_ref());
 
     window.set_title("Basstrace");
     window.set_border_width(10);
     window.set_position(gtk::WindowPosition::Center);
     window.set_default_size(1280, 720);
+
+    window.add(&image);
 
     window.show_all();
 }
